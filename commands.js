b0997fddc108e7027fa2d6e2f27fa147a2e181af
AngeloidIkaros <thomas.henne97@gmail.com>
2016-12-05 03:31:59 +0100

    Changed !history Message
    First time pushing something! Can i do it ?! Thanks MrLanCaster for the suggestion and Laquett for teaching Waddie English :D

--- commands.js
+++ commands.js
@@ -283,7 +283,7 @@ function regCommands(commandManager) {
                         utils.bot.sendChat((arg0 ? 'That' : 'This') + ' video/song was last played ' + moment(parseInt(result)).from(Date.now()) + '.');
                     }
                     else {
-                        utils.bot.sendChat((arg0 ? 'That' : 'This') + ' video/song has not played in the last 5 weeks. Maybe the song is a remix, or a reupload.');
+                        utils.bot.sendChat((arg0 ? 'That' : 'This') + ' video/song hasn't played in the past 5 weeks. It could never have played before, is a re-upload, or a remix.');
                     }
                 });
             }
